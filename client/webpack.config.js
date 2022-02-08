4f728022e496518825990829c172d2df990acb6f
sean <seansiddells@gmail.com>
2022-02-09 12:20:22 +1300

    add package.json and file structure

---
+++ client/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+  plugins: [
+    new Dotenv()
+  ],
+  entry: path.join(__dirname, './index.js'),
+  output: {
+    path: path.join(__dirname, '../server/public'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    fallback: {
+      fs: false
+    }
+  },
+  devtool: 'source-map'
+}
